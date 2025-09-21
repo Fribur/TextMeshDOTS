aa294c999fc3f3fda2026e803e526684ad0a4147
Tyler <dreamingimlatios@gmail.com>
2025-09-21 18:06:38 -0500

    Fix top ascent to bottom descent layout mode

--- Internal/GlyphGeneration.cs
+++ Internal/GlyphGeneration.cs
@@ -721,7 +721,7 @@ namespace TextMeshDOTS
                     }
                 case VerticalAlignmentOptions.MiddleTopAscentToBottomDescent:
                     {
-                        float fullHeight = accumulatedVerticalOffset - bottomAnchor + topAnchor;
+                        float fullHeight = accumulatedVerticalOffset - topAnchor - bottomAnchor;
                         float offset = fullHeight / 2f;
                         for (int i = 0; i < renderGlyphsOld.Length; i++)
                         {
